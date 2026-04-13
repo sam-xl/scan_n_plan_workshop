29dd5bd278e4ab9692a144028fc4ecf51bc13788
Nik Sethi <niksethi@tudelft.nl>
2026-04-13 13:50:31 +0000

    Inherit from IfopDefault instead of Ifopt

--- snp_motion_planning/src/trajopt_ifopt_custom_composite_profile.h
+++ snp_motion_planning/src/trajopt_ifopt_custom_composite_profile.h
@@ -37,73 +37,28 @@ TESSERACT_COMMON_IGNORE_WARNINGS_PUSH
 TESSERACT_COMMON_IGNORE_WARNINGS_POP
 
 #include <tesseract_motion_planners/trajopt/trajopt_collision_config.h>
-#include <tesseract_motion_planners/trajopt/profile/trajopt_profile.h>
+#include <tesseract_motion_planners/trajopt_ifopt/profile/trajopt_ifopt_default_composite_profile.h>
 
 #include <tesseract_collision/core/fwd.h>
 #include <tesseract_collision/core/types.h>
 
 namespace tesseract_planning
 {
-class TrajOptIfoptCustomCompositeProfile : public TrajOptCompositeProfile
+class TrajOptIfoptCustomCompositeProfile : public TrajOptIfoptDefaultCompositeProfile
 {
 public:
-  using Ptr = std::shared_ptr<TrajOptIfoptCustomCompositeProfile>;
-  using ConstPtr = std::shared_ptr<const TrajOptIfoptCustomCompositeProfile>;
-
   TrajOptIfoptCustomCompositeProfile() = default;
 
-  /** @brief The type of contact test to perform: FIRST, CLOSEST, ALL */
   tesseract_collision::ContactTestType contact_test_type{ tesseract_collision::ContactTestType::ALL };
-  /** @brief Configuration info for collisions that are modeled as costs */
   CollisionCostConfig collision_cost_config;
-  /** @brief Configuration info for collisions that are modeled as constraints */
   CollisionConstraintConfig collision_constraint_config;
-  /** @brief If true, a joint velocity cost with a target of 0 will be applied for all timesteps Default: true*/
-  bool smooth_velocities = true;
-  /** @brief This default to all ones, but allows you to weight different joints */
-  Eigen::VectorXd velocity_coeff{};
-  /** @brief If true, a joint acceleration cost with a target of 0 will be applied for all timesteps Default: false*/
-  bool smooth_accelerations = true;
-  /** @brief This default to all ones, but allows you to weight different joints */
-  Eigen::VectorXd acceleration_coeff{};
-  /** @brief If true, a joint jerk cost with a target of 0 will be applied for all timesteps Default: false*/
-  bool smooth_jerks = true;
-  /** @brief This default to all ones, but allows you to weight different joints */
-  Eigen::VectorXd jerk_coeff{};
-  /** @brief If true, applies a cost to avoid kinematic singularities */
-  bool avoid_singularity = false;
-  /** @brief Optimization weight associated with kinematic singularity avoidance */
-  double avoid_singularity_coeff = 5.0;
-
-  /** @brief Set the resolution at which state validity needs to be verified in order for a motion between two states
-   * to be considered valid in post checking of trajectory returned by trajopt.
-   *
-   * The resolution is equal to longest_valid_segment_fraction * state_space.getMaximumExtent()
-   *
-   * Note: The planner takes the conservative of either longest_valid_segment_fraction or longest_valid_segment_length.
-   */
-  double longest_valid_segment_fraction = 0.01;  // 1%
-
-  /** @brief Set the resolution at which state validity needs to be verified in order for a motion between two states
-   * to be considered valid. If norm(state1 - state0) > longest_valid_segment_length.
-   *
-   * Note: This gets converted to longest_valid_segment_fraction.
-   *       longest_valid_segment_fraction = longest_valid_segment_length / state_space.getMaximumExtent()
-   */
-  double longest_valid_segment_length = 0.1;
-
+  
   /**@brief Special link collision cost distances */
   std::shared_ptr<trajopt_common::SafetyMarginData> special_collision_cost{ nullptr };
   /**@brief Special link collision constraint distances */
   std::shared_ptr<trajopt_common::SafetyMarginData> special_collision_constraint{ nullptr };
 
-  TrajOptTermInfos create(const tesseract_common::ManipulatorInfo& composite_manip_info,
-                          const std::shared_ptr<const tesseract_environment::Environment>& env,
-                          const std::vector<int>& fixed_indices, int start_index, int end_index) const override;
-
 protected:
-  double computeLongestValidSegmentLength(const Eigen::MatrixX2d& joint_limits) const;
-
   friend class boost::serialization::access;
   template <class Archive>
   void serialize(Archive&, const unsigned int);  // NOLINT
